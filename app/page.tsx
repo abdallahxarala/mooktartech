938cad2d9b661a2c60156d99a2c606abff127a60
abdallahxarala <mooktech@gmail.com>
2025-11-24 22:38:27 +0000

    fix: Simplify root test page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,4 @@
-export default function RootPage() {
-  return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <h1>✅ Site Fonctionne</h1>
-      <p>Cette page de test charge correctement</p>
-      <div style={{ marginTop: '20px' }}>
-        <a href="/fr/org/foire-dakar-2025">Foire Dakar</a>
-        {' | '}
-        <a href="/fr/org/mooktartech-com">Mooktar</a>
-        {' | '}
-        <a href="/fr/org/xarala-solutions">Xarala</a>
-      </div>
-    </div>
-  )
+export default function TestPage() {
+  return <div><h1>TEST - Site Fonctionne</h1></div>
 }
 
